e695bebd4810e9aa28b29692fd817b1909b277bc
Sachin210894 <42200018+Sachin210894@users.noreply.github.com>
2026-02-03 17:50:50 +0000

    deploy love you

--- script.js
+++ script.js
@@ -1,9 +1,27 @@
+const photos = [
+  "photos/photo1.jpg",
+  "photos/photo2.jpg",
+  "photos/photo3.jpg"
+];
+
+let index = 0;
+const slide = document.getElementById("slide");
+
+setInterval(() => {
+  index = (index + 1) % photos.length;
+  slide.src = photos[index];
+}, 3000);
+
 const yesBtn = document.getElementById("yesBtn");
 const noBtn = document.getElementById("noBtn");
 const response = document.getElementById("response");
 
 yesBtn.addEventListener("click", () => {
-  response.innerHTML = "YAY!!! ❤️❤️❤️ I love you forever!";
+  response.innerHTML = `
+    You have my heart today,<br>
+    tomorrow, and forever. ❤️<br><br>
+    Happy Valentine’s Day, Tanuu.
+  `;
 });
 
 noBtn.addEventListener("mouseover", () => {
